63cb85e352cec8825f5b9c392c694a4e63110e72
MrPythonBrasil <tropadomess@gmail.com>
2025-03-12 14:45:33 -0300

    Update mainh.py

--- mainh.py
+++ mainh.py
@@ -1,64 +1,29 @@
-pip install colorama
+import requests
+import threading
 
-import os
-from colorama import Fore, Back, Style, init
+# URL do site que você deseja testar
+url = "https://cebacentroeducacional.com.br/ensino-medio/uso-de-celular-por-alunos-pode-ser-proibido-em-escolas-municipais-do-rio/#"
 
-# Inicializa o colorama
-init(autoreset=True)
+# Número de requisições que você deseja enviar
+num_requests = 100
 
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
+# Função para enviar uma requisição
+def send_request():
+    try:
+        response = requests.get(url)
+        print(f"Status Code: {response.status_code}")  # Mostra o código de status da resposta
+    except Exception as e:
+        print(f"Erro: {e}")
 
-def mostrar_titulo():
-    print(Fore.YELLOW + Back.BLACK + Style.BRIGHT + "Calculadora Super Eficiente!")
-    print(Fore.CYAN + "Escolha uma operação:")
+# Criando e iniciando as threads para enviar requisições simultaneamente
+threads = []
+for i in range(num_requests):
+    thread = threading.Thread(target=send_request)
+    threads.append(thread)
+    thread.start()
 
-def mostrar_opcoes():
-    print(Fore.GREEN + "1. Adição (+)")
-    print(Fore.GREEN + "2. Subtração (-)")
-    print(Fore.GREEN + "3. Multiplicação (*)")
-    print(Fore.GREEN + "4. Divisão (/)")
-    print(Fore.RED + "5. Sair")
+# Aguardando todas as threads terminarem
+for thread in threads:
+    thread.join()
 
-def realizar_calculo():
-    while True:
-        limpar_tela()
-        mostrar_titulo()
-        mostrar_opcoes()
-
-        try:
-            escolha = int(input(Fore.MAGENTA + "Digite o número da operação desejada: "))
-
-            if escolha == 5:
-                print(Fore.BLUE + "Saindo da calculadora. Até mais!")
-                break
-
-            if escolha < 1 or escolha > 5:
-                print(Fore.RED + "Opção inválida! Tente novamente.")
-                continue
-
-            num1 = float(input(Fore.CYAN + "Digite o primeiro número: "))
-            num2 = float(input(Fore.CYAN + "Digite o segundo número: "))
-
-            if escolha == 1:
-                resultado = num1 + num2
-                print(Fore.GREEN + f"O resultado de {num1} + {num2} é: {resultado}")
-            elif escolha == 2:
-                resultado = num1 - num2
-                print(Fore.GREEN + f"O resultado de {num1} - {num2} é: {resultado}")
-            elif escolha == 3:
-                resultado = num1 * num2
-                print(Fore.GREEN + f"O resultado de {num1} * {num2} é: {resultado}")
-            elif escolha == 4:
-                if num2 == 0:
-                    print(Fore.RED + "Erro! Divisão por zero.")
-                else:
-                    resultado = num1 / num2
-                    print(Fore.GREEN + f"O resultado de {num1} / {num2} é: {resultado}")
-        except ValueError:
-            print(Fore.RED + "Entrada inválida! Por favor, digite números válidos.")
-        
-        input(Fore.YELLOW + "\nPressione Enter para continuar...")
-
-if __name__ == "__main__":
-    realizar_calculo()
+print("Teste concluído!")
